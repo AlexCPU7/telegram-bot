7fbd9b7bfc8082a86693d87c200545a220d819b2
alexcpu7@gmail.com <Ja54So67N>
2019-06-29 19:29:04 +0300

    fix bug

--- job_monitoring_bot/hh/__init__.py
+++ job_monitoring_bot/hh/__init__.py
@@ -1,3 +1,3 @@
-from .logic import (get_search_vacancies,
-                    get_search_count_vacancy,
-                    get_search_count_vacancies)
+from job_monitoring_bot.hh.logic import (get_search_vacancies,
+                                         get_search_count_vacancy,
+                                         get_search_count_vacancies)
--- job_monitoring_bot/script_push_bot.py
+++ job_monitoring_bot/script_push_bot.py
@@ -8,19 +8,69 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(BASE_DIR)
 sys.path.insert(0, BASE_DIR)
 
-from job_monitoring_bot.config import MAIN_URL
-from job_monitoring_bot.hh.logic import get_search_count_vacancies
-
-
+TOKEN = ''
+MAIN_URL = f'https://api.telegram.org/bot{TOKEN}'
 BASE_URL = 'https://api.hh.ru/'
-CONNECT_TIMEOUT = 60
-READ_TIMEOUT = 60
+CONNECT_TIMEOUT = 10
+READ_TIMEOUT = 10
 TIME_PUSH = '18:00'
 ARR_JOBS = ('python', 'django', 'tornado', 'asyncio', 'aiohttp',
             'javascript', 'react', 'node.js',
             'java', 'string',
             'kotlin',
             'golang')
+
+
+def get_search_vacancies(text=None):
+    """
+    Returns all job information by search
+    :param text: (str)
+    :return: (int)
+    """
+    url = '{}vacancies?text={}&area=1&from=cluster_area'.format(BASE_URL, text)
+    try:
+        r = requests.get(url, timeout=(CONNECT_TIMEOUT, READ_TIMEOUT))
+    except requests.exceptions.ReadTimeout:
+        raise Exception('Истек таймаут чтения')
+    except requests.exceptions.ConnectTimeout:
+        raise Exception('Истекло время ожидания соединения c сервером')
+    except requests.exceptions.ConnectionError:
+        raise Exception('Неудалось подключится к серверу')
+    except requests.exceptions.HTTPError as err:
+        raise Exception('Произошла ошибка HTTP: {}'.format(err.response.content))
+
+    try:
+        data = r.json()
+    except Exception as arr:
+        raise Exception(arr)
+
+    return data
+
+
+def get_search_count_vacancy(text=None):
+    """
+    Returns the number of vacancies by search
+    :param text: (str)
+    :return: (int)
+    """
+    data = get_search_vacancies(text)
+    if isinstance(data, dict):
+        return data.get('found')
+    else:
+        raise Exception('Полученные данные не являются словарем')
+
+
+def get_search_count_vacancies(name_vacancies):
+    """
+    Get the number of vacancies for each search.
+    :param name_vacancies: (list)
+    :return: (dist)
+    """
+    jobs = {}
+    for name_vacancy in name_vacancies:
+        count_vacancy = get_search_count_vacancy(name_vacancy)
+        jobs.update({name_vacancy: count_vacancy})
+    return jobs
 
 
 def main():
